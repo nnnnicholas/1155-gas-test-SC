19dc66a71274781182d60b98202fde3dcfe97f48
nicholas <nnnnicholas@users.noreply.github.com>
2022-05-19 10:05:24 -0400

    added onlyowner in case anyone copies the code ahah

--- src/Contract.sol
+++ src/Contract.sol
@@ -2,8 +2,9 @@
 pragma solidity >=0.8.0;
 
 import "solmate/tokens/ERC1155.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract Contract is ERC1155 {
+contract Contract is ERC1155, Ownable {
     constructor() ERC1155() {}
 
     function mint(
@@ -11,7 +12,7 @@ contract Contract is ERC1155 {
         uint256 id,
         uint256 amount,
         bytes memory data
-    ) public {
+    ) public onlyOwner {
         ERC1155._mint(to, id, amount, data);
     }
 
@@ -20,16 +21,20 @@ contract Contract is ERC1155 {
         uint256[] memory ids,
         uint256[] memory amounts,
         bytes memory data
-    ) external {
+    ) external onlyOwner {
         ERC1155._batchMint(to, ids, amounts, data);
     }
 
+    // optimized
     function batchMintRewrite1Each(address[] memory recipients, uint256 id)
         external
+        onlyOwner
     {
         uint256 amount = recipients.length;
         for (uint256 i = 0; i < amount; i++) {
-            balanceOf[recipients[i]][id]++;
+            unchecked {
+                balanceOf[recipients[i]][id]++;
+            }
             emit TransferSingle(msg.sender, address(0), recipients[i], id, 1);
         }
     }
@@ -38,7 +43,7 @@ contract Contract is ERC1155 {
         return "URI";
     }
 
-    function testMintOneThousand() public {
+    function testMintOneThousand() public onlyOwner {
         for (uint160 i = 0; i < 1000; i++) {
             mint(address(i), 1, 1, "");
         }
